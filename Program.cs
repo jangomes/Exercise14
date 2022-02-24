7642d00d5d7f8d25905e05b9e04cb32425ad0b01
Janaina Gomes da Silva <21944@student.dorset-college.ie>
2022-02-24 13:20:22 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise14
+{
+    class Program
+    {
+        /*  Create a console-based application whose Main() method asks the user to input an integer 
+            and then calls a method named MultiplicationTable(), which displays the results of 
+            multiplying the integer by each of the numbers 2 through 10*/
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a integer: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 2; i < 11; i++)
+            {
+                int answer = input1 * i;
+                Console.WriteLine("{0,2} * {1,2} = {2,2}", i, input1, answer);
+            }
+        }
+
+    }
+}
